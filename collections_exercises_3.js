7e1251ace8db5ed7a83d3ff8e7bb6007a52e4622
Linus Phan <phanlinus@gmail.com>
2019-07-02 23:11:42 -0700

    work on more collection exercises and glimpse into "callback hell", look at parse, sort, comparator, etc.

--- collections_exercises_3.js
+++ collections_exercises_3.js
@@ -1,3 +1,5 @@
+var template = Handlebars.compile($('#users').html());
+
 var User = Backbone.Model.extend({
   url: 'http://jsonplaceholder.typicode.com/users',
 })
@@ -5,6 +7,44 @@ var User = Backbone.Model.extend({
 var Users = Backbone.Collection.extend({
   url: 'http://jsonplaceholder.typicode.com/users',
   model: User,
+
+  parse: function (response) {
+    response.forEach(function (user) {
+      user.companyName = user.company.name;
+      user.catchPhrase = user.company.catchPhrase;
+      user.companyBs = user.company.bs;
+
+      delete user.company;
+    });
+
+    return response;
+  },
+
+  initialize: function () {
+    this.on('sync sort', renderCollection);
+  },
 });
 
+function renderCollection() {
+  document.body.innerHTML = template({ users: blogWriters.toJSON() });
+}
 
+var blogWriters = new Users();
+
+// blogWriters.fetch({
+//   success: function () {
+//     blogWriters.create({ name: 'John Doe', email: 'JohnDoe@example.com' }, {
+      
+//       success: function () {
+//         blogWriters.fetch({
+//           reset: true,
+//           success: function () {
+//             console.log(blogWriters.toJSON());
+//           },
+//         });
+//       },
+//     });
+//   },
+// });
+
+blogWriters.fetch();
